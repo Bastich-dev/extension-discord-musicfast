836a98a02eacdd704f8089ec036bddafbef9aca5
BastosC <bastien.chantrel.pro@gmail.com>
2023-06-22 10:15:31 +0200

    setup next confi

--- next.config.js
+++ next.config.js
@@ -5,8 +5,16 @@ const nextConfig = {
             {
                 source: "/api/(.*)",
                 headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Same Origin", value: false },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
                 ],
             },
         ];

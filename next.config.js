102f8c6f94f629163d6fab44f0582f628a0b7211
BastosC <bastien.chantrel.pro@gmail.com>
2023-06-22 10:41:16 +0200

    tod

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                headers: [
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-                    },
-                ],
-            },
-        ];
-    },
+    // async headers() {
+    //     return [
+    //         {
+    //             headers: [
+    //                 { key: "Access-Control-Allow-Origin", value: "*" },
+    //                 {
+    //                     key: "Access-Control-Allow-Headers",
+    //                     value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    //                 },
+    //             ],
+    //         },
+    //     ];
+    // },
 };
 
 module.exports = nextConfig;
